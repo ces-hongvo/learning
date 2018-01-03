ca06dde304a3e3960498f1e64c25aae4e16faa02
hongvo2308 <hong.vo@codeenginestudio.com>
2018-01-03 14:08:18 +0700

    init

---
+++ Algorithms/binary_string.py
@@ -0,0 +1,20 @@
+max = 30
+x = []
+n = 5
+for i in range(n):
+    x.append(0)
+
+print(x)
+i = n-1
+while (True):
+    i = n-1;
+    while (i >= 0 and x[i] == 1): 
+        i = i - 1
+
+    if (i>=0):
+        x[i] = 1
+        for j in range(i+1, n):
+            x[j] = 0
+        print(x)
+    else :
+        break
